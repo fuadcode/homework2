395982e3906f2680df575215c7a74e3dee58dfb0
Iskandarli Fuad <fuadgi@code.edu.az>
2023-10-08 16:29:23 +0400

    Homework finished

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1 = 4;
+int num2 = 5;
+//
+num1 = num1 + num2;
+num2 = num1 - num2;
+num1 = num1 - num2;
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
